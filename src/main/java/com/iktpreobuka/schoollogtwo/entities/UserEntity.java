a3122688867222d39b02c46f1deda1e323796c73
pedja77 <pedja77@stcable.net>
2023-03-05 17:43:48 +0100

    Cahnges to UserEntity

--- src/main/java/com/iktpreobuka/schoollogtwo/entities/UserEntity.java
+++ src/main/java/com/iktpreobuka/schoollogtwo/entities/UserEntity.java
@@ -21,7 +21,6 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.annotations.Where;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
